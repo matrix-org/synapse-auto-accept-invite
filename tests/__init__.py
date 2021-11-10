937fd3a9e5c3971bd598d48dc5dfa38ff74bbf04
Brendan Abolivier <babolivier@matrix.org>
2021-11-10 18:40:13 +0100

    Add tests

---
+++ tests/__init__.py
@@ -0,0 +1,60 @@
+# Copyright 2021 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from asyncio import Future
+from typing import Any, Awaitable, Optional
+from unittest.mock import Mock
+
+import attr
+from synapse.module_api import ModuleApi
+
+from synapse_auto_accept_invite import InviteAutoAccepter
+
+
+@attr.s(auto_attribs=True)
+class MockEvent:
+    """Mocks an event. Only exposes properties the module uses."""
+    sender: str
+    type: str
+    content: dict
+    room_id: str = "!someroom"
+    state_key: Optional[str] = None
+
+    def is_state(self):
+        """Checks if the event is a state event by checking if it has a state key."""
+        return self.state_key is not None
+
+    @property
+    def membership(self):
+        """Extracts the membership from the event. Should only be called on an event
+        that's a membership event, and will raise a KeyError otherwise.
+        """
+        return self.content["membership"]
+
+
+def create_module() -> InviteAutoAccepter:
+    # Create a mock based on the ModuleApi spec, but override some mocked functions
+    # because some capabilities are needed for running the tests.
+    module_api = Mock(spec=ModuleApi)
+    module_api.is_mine.side_effect = lambda a: a.split(":")[1] == "test"
+
+    # Python 3.6 doesn't support awaiting on a mock, so we make it return an awaitable
+    # value.
+    def make_awaitable(result: Any) -> Awaitable[Any]:
+        future = Future()  # type: ignore
+        future.set_result(result)
+        return future
+
+    module_api.update_room_membership.return_value = make_awaitable(None)
+
+    return InviteAutoAccepter({}, module_api)
---
+++ tests/test_example.py
@@ -0,0 +1,103 @@
+# Copyright 2021 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import aiounittest
+
+from tests import create_module, MockEvent
+
+
+class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
+    def setUp(self) -> None:
+        self.module = create_module()
+        self.user_id = "@peter:test"
+        self.invitee = "@lesley:test"
+        self.remote_invitee = "@thomas:remote"
+
+    async def test_accept_invite(self) -> None:
+        """Tests that receiving an invite for a local user makes the module attempt to
+        make the invitee join the room.
+        """
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.invitee,
+            type="m.room.member",
+            content={"membership": "invite"}
+        )
+
+        await self.module.on_new_event(event=invite)
+
+        # Check that the mocked method is called exactly once.
+        self.module._api.update_room_membership.assert_called_once()
+
+        # Check that the mocked method is called with the right arguments to attempt to
+        # make the user join the room.
+        kwargs = self.module._api.update_room_membership.call_args[1]
+        self.assertEqual(kwargs["sender"], invite.state_key)
+        self.assertEqual(kwargs["target"], invite.state_key)
+        self.assertEqual(kwargs["room_id"], invite.room_id)
+        self.assertEqual(kwargs["new_membership"], "join")
+
+    async def test_remote_user(self) -> None:
+        """Tests that receiving an invite for a remote user does nothing."""
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.remote_invitee,
+            type="m.room.member",
+            content={"membership": "invite"}
+        )
+
+        await self.module.on_new_event(event=invite)
+
+        self.assertEqual(self.module._api.update_room_membership.call_count, 0)
+
+    async def test_not_state(self) -> None:
+        """Tests that receiving an invite that's not a state event does nothing."""
+        invite = MockEvent(
+            sender=self.user_id,
+            type="m.room.member",
+            content={"membership": "invite"}
+        )
+
+        await self.module.on_new_event(event=invite)
+
+        self.assertEqual(self.module._api.update_room_membership.call_count, 0)
+
+    async def test_not_invite(self) -> None:
+        """Tests that receiving a membership update that's not an invite does nothing."""
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.user_id,
+            type="m.room.member",
+            content={"membership": "join"}
+        )
+
+        await self.module.on_new_event(event=invite)
+
+        self.assertEqual(self.module._api.update_room_membership.call_count, 0)
+
+    async def test_not_membership(self) -> None:
+        """Tests that receiving a state event that's not a membership update does
+        nothing.
+        """
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.user_id,
+            type="org.matrix.test",
+            content={"foo": "bar"}
+        )
+
+        await self.module.on_new_event(event=invite)
+
+        self.assertEqual(self.module._api.update_room_membership.call_count, 0)
+
+
