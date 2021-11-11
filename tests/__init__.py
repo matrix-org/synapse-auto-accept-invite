dff8f0f865f40a0cbf285d7c0630bce7cb318a60
Brendan Abolivier <babolivier@matrix.org>
2021-11-11 11:50:14 +0100

    Import order

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from asyncio import Future
-from typing import Any, Awaitable, Optional, Dict
+from typing import Any, Awaitable, Dict, Optional
 from unittest.mock import Mock
 
 import attr
