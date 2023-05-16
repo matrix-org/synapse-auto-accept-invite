936525a43fb00d44dc7c44473b4ab74656fa457d
Shay <hillerys@element.io>
2023-05-16 10:59:32 -0700

    Fix invite acceptance over federation (#15)

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Dict, Optional, TypeVar
+import asyncio
+from asyncio import Future
+from typing import Any, Awaitable, Dict, Optional, TypeVar
 from unittest.mock import Mock
 
 import attr
-from synapse.module_api import ModuleApi
+from synapse.module_api import ModuleApi, run_as_background_process
 
 from synapse_auto_accept_invite import InviteAutoAccepter
 
@@ -44,10 +46,22 @@ class MockEvent:
 
 
 T = TypeVar("T")
+TV = TypeVar("TV")
 
 
 async def make_awaitable(value: T) -> T:
     return value
+
+
+def make_multiple_awaitable(result: TV) -> Awaitable[TV]:
+    """
+    Makes an awaitable, suitable for mocking an `async` function.
+    This uses Futures as they can be awaited multiple times so can be returned
+    to multiple callers. Stolen from synapse.
+    """
+    future: Future[TV] = Future()
+    future.set_result(result)
+    return future
 
 
 def create_module(
@@ -58,10 +72,14 @@ def create_module(
     module_api = Mock(spec=ModuleApi)
     module_api.is_mine.side_effect = lambda a: a.split(":")[1] == "test"
     module_api.worker_name = worker_name
+    module_api.sleep.return_value = make_multiple_awaitable(None)
 
-    # Python 3.6 doesn't support awaiting on a mock, so we make it return an awaitable
-    # value.
-    module_api.update_room_membership.return_value = make_awaitable(None)
     config = InviteAutoAccepter.parse_config(config_override)
+
+    run_as_background_process.side_effect = (
+        lambda desc, func, *args, bg_start_span, **kwargs: asyncio.create_task(
+            func(*args, **kwargs)
+        )
+    )
 
     return InviteAutoAccepter(config, module_api)
--- tests/test_accept_invite.py
+++ tests/test_accept_invite.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import cast
+import asyncio
+from typing import Any, cast
 from unittest.mock import Mock
 
 import aiounittest
@@ -31,9 +32,10 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
         # We know our module API is a mock, but mypy doesn't.
         self.mocked_update_membership: Mock = self.module._api.update_room_membership  # type: ignore[assignment]
 
-    async def test_accept_invite(self) -> None:
+    async def test_simple_accept_invite(self) -> None:
         """Tests that receiving an invite for a local user makes the module attempt to
-        make the invitee join the room.
+        make the invitee join the room. This test verifies that it works if the call to
+        update membership returns a join event on the first try.
         """
         invite = MockEvent(
             sender=self.user_id,
@@ -42,13 +44,86 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
             content={"membership": "invite"},
         )
 
+        join_event = MockEvent(
+            sender="someone",
+            state_key="someone",
+            type="m.room.member",
+            content={"membership": "join"},
+        )
+        self.mocked_update_membership.return_value = make_awaitable(join_event)
+
         # Stop mypy from complaining that we give on_new_event a MockEvent rather than an
         # EventBase.
         await self.module.on_new_event(event=invite)  # type: ignore[arg-type]
 
-        # Check that the mocked method is called exactly once and with the right
-        # arguments to attempt to make the user join the room.
-        self.mocked_update_membership.assert_called_once_with(
+        await self.retry_assertions(
+            self.mocked_update_membership,
+            1,
+            sender=invite.state_key,
+            target=invite.state_key,
+            room_id=invite.room_id,
+            new_membership="join",
+        )
+
+    async def test_accept_invite_with_failures(self) -> None:
+        """Tests that receiving an invite for a local user makes the module attempt to
+        make the invitee join the room. This test verifies that it works if the call to
+        update membership returns exceptions before successfully completing and returning an event.
+        """
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.invitee,
+            type="m.room.member",
+            content={"membership": "invite"},
+        )
+
+        join_event = MockEvent(
+            sender="someone",
+            state_key="someone",
+            type="m.room.member",
+            content={"membership": "join"},
+        )
+        # the first two calls raise an exception while the third call is successful
+        self.mocked_update_membership.side_effect = [
+            Exception(),
+            Exception(),
+            make_awaitable(join_event),
+        ]
+
+        # Stop mypy from complaining that we give on_new_event a MockEvent rather than an
+        # EventBase.
+        await self.module.on_new_event(event=invite)  # type: ignore[arg-type]
+
+        await self.retry_assertions(
+            self.mocked_update_membership,
+            3,
+            sender=invite.state_key,
+            target=invite.state_key,
+            room_id=invite.room_id,
+            new_membership="join",
+        )
+
+    async def test_accept_invite_failures(self) -> None:
+        """Tests that receiving an invite for a local user makes the module attempt to
+        make the invitee join the room. This test verifies that if the update_membership call
+        fails consistently, _retry_make_join will break the loop after the set number of retries and
+        execution will continue.
+        """
+        invite = MockEvent(
+            sender=self.user_id,
+            state_key=self.invitee,
+            type="m.room.member",
+            content={"membership": "invite"},
+        )
+        self.mocked_update_membership.side_effect = Exception()
+
+        # Stop mypy from complaining that we give on_new_event a MockEvent rather than an
+        # EventBase.
+        await self.module.on_new_event(event=invite)  # type: ignore[arg-type]
+
+        await self.retry_assertions(
+            self.mocked_update_membership,
+            5,
             sender=invite.state_key,
             target=invite.state_key,
             room_id=invite.room_id,
@@ -67,6 +142,14 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
             content={"membership": "invite", "is_direct": True},
             room_id="!the:room",
         )
+
+        join_event = MockEvent(
+            sender="someone",
+            state_key="someone",
+            type="m.room.member",
+            content={"membership": "join"},
+        )
+        self.mocked_update_membership.return_value = make_awaitable(join_event)
 
         # We will mock out the account data get/put methods to check that the flags
         # are properly set.
@@ -90,9 +173,9 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
         # EventBase.
         await self.module.on_new_event(event=invite)  # type: ignore[arg-type]
 
-        # Check that the mocked method is called exactly once and with the right
-        # arguments to attempt to make the user join the room.
-        self.mocked_update_membership.assert_called_once_with(
+        await self.retry_assertions(
+            self.mocked_update_membership,
+            1,
             sender=invite.state_key,
             target=invite.state_key,
             room_id=invite.room_id,
@@ -188,6 +271,15 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
         account_data_get: Mock = cast(Mock, module._api.account_data_manager.get_global)
         account_data_get.return_value = make_awaitable({})
 
+        mocked_update_membership: Mock = module._api.update_room_membership  # type: ignore[assignment]
+        join_event = MockEvent(
+            sender="someone",
+            state_key="someone",
+            type="m.room.member",
+            content={"membership": "join"},
+        )
+        mocked_update_membership.return_value = make_awaitable(join_event)
+
         invite = MockEvent(
             sender=self.user_id,
             state_key=self.invitee,
@@ -199,10 +291,9 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
         # EventBase.
         await module.on_new_event(event=invite)  # type: ignore[arg-type]
 
-        # Check that the mocked method is called exactly once and with the right
-        # arguments to attempt to make the user join the room.
-        mocked_update_membership: Mock = module._api.update_room_membership  # type: ignore[assignment]
-        mocked_update_membership.assert_called_once_with(
+        await self.retry_assertions(
+            mocked_update_membership,
+            1,
             sender=invite.state_key,
             target=invite.state_key,
             room_id=invite.room_id,
@@ -262,3 +353,32 @@ class InviteAutoAccepterTestCase(aiounittest.AsyncTestCase):
         cast(
             Mock, specified_module._api.register_third_party_rules_callbacks
         ).assert_called_once()
+
+    async def retry_assertions(
+        self, mock: Mock, call_count: int, **kwargs: Any
+    ) -> None:
+        """
+        This is a hacky way to ensure that the assertions are not called before the other coroutine
+        has a chance to call `update_room_membership`. It catches the exception caused by a failure,
+        and sleeps the thread before retrying, up until 5 tries.
+
+        Args:
+            call_count: the number of times the mock should have been called
+            mock: the mocked function we want to assert on
+            kwargs: keyword arguments to assert that the mock was called with
+        """
+
+        i = 0
+        while i < 5:
+            try:
+                # Check that the mocked method is called the expected amount of times and with the right
+                # arguments to attempt to make the user join the room.
+                mock.assert_called_with(**kwargs)
+                self.assertEqual(call_count, mock.call_count)
+                break
+            except AssertionError as e:
+                i += 1
+                if i == 5:
+                    # we've used up the tries, force the test to fail as we've already caught the exception
+                    self.fail(e)
+                await asyncio.sleep(1)
